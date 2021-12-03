e0791a0dcf7692120d8e75ac23fa3fdcd3fd8d3a
tiffany1618 <tiffany9342@gmail.com>
2021-12-02 22:47:59 -0800

    Updated benchmarks

--- benches/imgproc_bench.rs
+++ benches/imgproc_bench.rs
@@ -5,23 +5,19 @@ use imgproc_rs::{colorspace, tone};
 
 use criterion::{criterion_group, criterion_main, Criterion};
 
-// pub fn bench_brightness(c: &mut Criterion) {
-//     let img = setup("images/tux.png").unwrap();
-//
-//     c.bench_function("brightness rgb", |b| b.iter(||
-//         tone::brightness_rgb_norm(&img, 20)));
-//     c.bench_function("brightness rgb avx2", |b| b.iter(||
-//         tone::brightness(&img, 20, Tone::Rgb)));
-// }
+pub fn bench_brightness(c: &mut Criterion) {
+    let img = setup("images/tux.png").unwrap();
 
-// pub fn bench_rgb_to_grayscale(c: &mut Criterion) {
-//     let img = setup("images/tux.png").unwrap();
-//
-//     c.bench_function("avx2", |b| b.iter(||
-//         colorspace::rgb_to_grayscale(&img)));
-//     c.bench_function("norm", |b| b.iter(||
-//         colorspace::rgb_to_grayscale_norm(&img)));
-// }
+    c.bench_function("brightness", |b| b.iter(||
+        tone::brightness(&img, 20)));
+}
+
+pub fn bench_rgb_to_grayscale(c: &mut Criterion) {
+    let img = setup("images/tux.png").unwrap();
+
+    c.bench_function("grayscale", |b| b.iter(||
+        colorspace::rgb_to_grayscale(&img)));
+}
 
 pub fn bench_rgb_to_hsv(c: &mut Criterion) {
     let img = setup("images/spectrum.jpg").unwrap();
